8b0a855bd6cc801ee10f318e236f7f9504d2ab7d
Elyh Lapetina <evl12@case.edu>
2020-06-21 09:18:18 -0700

    Test new psot

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
+
+
 app.get('/', (req, res) => {
   res.send('Commit this!')
 })
+
+app.post('/post-test', (req, res) => {
+    console.log('Got body:', req.body);
+    res.sendStatus(200);
+});
+
+
+
 app.listen(3000, () => console.log('Server running on port 3000'))
