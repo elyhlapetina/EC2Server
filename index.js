57234fc36e6764f0c818a6adc35f0559ac96291e
Elyh Lapetina <evl12@case.edu>
2020-06-23 20:22:09 -0700

    Test sockets

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 
+const WebSocket = require('ws') 
+const wss = new WebSocket.Server({ port: 8080 })
+
 let fileScope = "Can be accessed anywhere in the file";
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,7 +18,12 @@ app.post('/post-test', (req, res) => {
     console.log('Got body:', req.body.message);
     res.sendStatus(200);
 });
-
-
+ 
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log(`Received message => ${message}`)
+  })
+  ws.send('Hello! Message From Server!!')
+})
 
 app.listen(3000, () => console.log('Server running on port 3000'))
